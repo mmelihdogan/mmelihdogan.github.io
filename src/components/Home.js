2db8ebcd9b5a8f7144ddb702323eb1ab9e82634a
mmelihdogan <42299017+mmelihdogan@users.noreply.github.com>
2020-04-27 18:18:30 +0300

    translation feature added to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,14 +4,19 @@ import { Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from './Navigation';
 
+import { useTranslation } from 'react-i18next';
+
+
 function Home() {
+    const { t } = useTranslation();
+
     return (
         <>
             <Navigation path="/" />
             <Container fluid className="Home">
                 <Container className="melih">
                     <h1>melih ;D</h1>
-                    <p>Apprentice Web Developer</p>
+                    <p>{t('home.title')}</p>
                 </Container>
             </Container >
         </>
