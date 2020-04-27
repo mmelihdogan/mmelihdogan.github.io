efa21f01d70c07478add58f1bb884728b7d21bab
mmelihdogan <42299017+mmelihdogan@users.noreply.github.com>
2020-04-27 18:24:42 +0300

    translation feature added to nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,12 +6,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { faIgloo, faCat, faPaw, faDove } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+import { useTranslation } from 'react-i18next';
+
+
 function Navigation(props) {
+    const { t } = useTranslation();
+
     const pages = [
-        { name: "Home", path: "/", icon: faIgloo },
-        { name: "About Me!", path: "/about", icon: faCat },
-        { name: "Portfolio", path: "/portfolio", icon: faPaw },
-        { name: "Contact", path: "/contact", icon: faDove },
+        { name: t('nav.home'), path: "/", icon: faIgloo },
+        { name: t('nav.about'), path: "/about", icon: faCat },
+        { name: t('nav.portfolio'), path: "/portfolio", icon: faPaw },
+        { name: t('nav.contact'), path: "/contact", icon: faDove },
     ];
 
     return (
